cd0caccbd5058a966d7d4c3d3505fef238afd524
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-12-22 18:30:30 +0100

    eureka client example

---
+++ src/test/java/com/soprasteria/eurekaclientexample/EurekaclientexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soprasteria.eurekaclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaclientexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
